eb07a1904bf9109c18272e439334cf47e215c3c4
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2022-03-16 12:41:40 +0200

    SrtData parse complete

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,37 @@
 </template>
 
 <script>
+
+const { default: srtParser2 } = require("srt-parser-2");
+
 export default {
-  name: 'IndexPage'
+  data(){
+    return {
+      subtitleFile: "https://res.cloudinary.com/hackit-africa/raw/upload/v1647426873/nuxtjs-video-subtitles/subtitles.srt",
+      subtitlesArray: null
+    }
+  },
+  async mounted(){
+    const srtData = await this.readSubtitleFile();
+    this.parseSrtData(srtData);
+  },
+  methods:{
+    readSubtitleFile(){
+      return new Promise(async (resolve, reject) => {
+        const blob = await (await fetch(this.subtitleFile)).blob();
+        const reader = new FileReader();
+        reader.readAsText(blob);
+        reader.addEventListener(
+          'load', 
+          (e) => resolve(e.target.result)
+        );
+      });
+    },
+    parseSrtData(srtData){
+      var parser = new srtParser2()
+      this.subtitlesArray =  parser.fromSrt(srtData);
+      console.log(this.subtitlesArray);
+    }
+  }
 }
 </script>
