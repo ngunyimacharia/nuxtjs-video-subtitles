4db6188bac7f3b6ddfb2f6fe92e1c4727c73b16a
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2022-03-16 13:21:30 +0200

    Subtitle display complete

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,16 @@
 <template>
-  <Tutorial/>
+  <cld-video :publicId="video" controls="true" autoplay="true" >
+    <cld-transformation :overlay="{publicId: subtitle, resourceType: 'subtitles'}" />
+    <cld-transformation flags="layer_apply" />
+  </cld-video>
 </template>
 
 <script>
-
-const { default: srtParser2 } = require("srt-parser-2");
-
 export default {
   data(){
     return {
-      subtitleFile: "https://res.cloudinary.com/hackit-africa/raw/upload/v1647426873/nuxtjs-video-subtitles/subtitles.srt",
-      subtitlesArray: null
-    }
-  },
-  async mounted(){
-    const srtData = await this.readSubtitleFile();
-    this.parseSrtData(srtData);
-  },
-  methods:{
-    readSubtitleFile(){
-      return new Promise(async (resolve, reject) => {
-        const blob = await (await fetch(this.subtitleFile)).blob();
-        const reader = new FileReader();
-        reader.readAsText(blob);
-        reader.addEventListener(
-          'load', 
-          (e) => resolve(e.target.result)
-        );
-      });
-    },
-    parseSrtData(srtData){
-      var parser = new srtParser2()
-      this.subtitlesArray =  parser.fromSrt(srtData);
-      console.log(this.subtitlesArray);
+      video:"nuxtjs-video-subtitles/video",
+      subtitle:"nuxtjs-video-subtitles/subtitles.srt",
     }
   }
 }
